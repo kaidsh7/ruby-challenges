4c5fd0eff206daafba397067a67b0fe866f49deb
kaids <kdshajrah07@gmail.com>
2017-05-14 16:08:57 -0400

    added atom.rb file

---
+++ atom.rb
@@ -0,0 +1,11 @@
+
+hash = {name: "kelly",
+        email: "kelly.wonderful@gmail.com",
+        }
+
+puts hash[:name]
+
+puts hash[:email]
+
+puts "added one line to experiment #{test}"
+puts "typing some bullshit here"
